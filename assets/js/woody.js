f9d0c99e044302e6473d35e7c33308a4bc735785
hagigataghazada <133520762+hagigataghazada@users.noreply.github.com>
2024-04-09 14:54:38 +0400

    swipers

--- assets/js/woody.js
+++ assets/js/woody.js
@@ -1,12 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
